5383fe7e9fe6033aa50fd1218f74120c83b40891
Maxim Donskoy <maxwebdevelop@yandex.ru>
2022-06-30 07:18:23 +0300

    add login form and base validation

--- src/app/layouts/login.jsx
+++ src/app/layouts/login.jsx
@@ -1,7 +1,66 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
+import TextField from "../components/textField";
+import { validator } from "../../utils/validator";
 
 const Login = () => {
-  return <h1>Login Page</h1>;
+  const [data, setData] = useState({ email: "", password: "" });
+  const [errors, setErrors] = useState({});
+
+  const handleChange = ({ target }) => {
+    setData((prevState) => ({ ...prevState, [target.name]: target.value }));
+  };
+
+  useEffect(() => {
+    validate();
+  }, [data]);
+
+  const validatorConfig = {
+    email: {
+      isRequired: { message: "Электронная почта обязательна для заполнения" },
+      isEmail: { message: "Email введен неккоректно" }
+    },
+    password: {
+      isRequired: { message: "Пароль обязателен для заполнения" },
+      isCapitalSymbol: {
+        message: "Пароль должен соджать хотя бы одну заглавную букву"
+      }
+    }
+  };
+
+  const validate = () => {
+    const errors = validator(data, validatorConfig);
+
+    setErrors(errors);
+    return Object.keys(errors).length === 0;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const isValid = validate();
+    if (!isValid) return;
+    console.log(data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <TextField
+        label="Электронная почта"
+        value={data.email}
+        name="email"
+        onChange={handleChange}
+        error={errors.email}
+      />
+      <TextField
+        label="Пароль"
+        type="password"
+        value={data.password}
+        name="password"
+        onChange={handleChange}
+        error={errors.password}
+      />
+      <button type="submit">submit</button>
+    </form>
+  );
 };
 
 export default Login;
---
+++ src/utils/validator.js
@@ -0,0 +1,44 @@
+/* eslint-disable indent */
+export function validator(data, config) {
+  const errors = {};
+  function validate(validateMethod, data, config) {
+    let statusValidate;
+    switch (validateMethod) {
+      case "isRequired":
+        statusValidate = data.trim() === "";
+        break;
+      case "isEmail": {
+        const emailRegX = /^\S+@\S+\.\S+$/g;
+        statusValidate = !emailRegX.test(data);
+        break;
+      }
+      case "isCapitalSymbol": {
+        const capitalRegx = /[A-Z]+/g;
+        statusValidate = !capitalRegx.test(data);
+        break;
+      }
+      case "isMinLength": {
+        const is = //g;
+        if (!capitalRegx.test(data)) return config.message;
+        break;
+      }
+
+      default:
+        break;
+    }
+    if (statusValidate) return config.message;
+  }
+  for (const fieldName in data) {
+    for (const validateMethod in config[fieldName]) {
+      const error = validate(
+        validateMethod,
+        data[fieldName],
+        config[fieldName][validateMethod]
+      );
+      if (error && !errors[fieldName]) {
+        errors[fieldName] = error;
+      }
+    }
+  }
+  return errors;
+}
